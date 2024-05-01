5ffe3b0102634f039caae6635c8de0d01a5718bb
dancey-apple <dancey.apple@proton.me>
2024-05-01 18:38:26 -0500

    working todo list

--- index.js
+++ index.js
@@ -1,16 +1,52 @@
 import prompts from 'prompts';
+import { color, log, cyan, magenta} from "console-log-colors";
+
+var todoList = [];
 
 const main = async () => {
-    const nextToDo = async() => {
+    const toDoMenu = async() => {
         return await prompts({
-            type: 'text',
+            type: 'select',
             name: 'value',
-            message: 'What is your next todo item?',
+            message: 'What would you like to do?',
+            choices: [
+                { title: 'View Todos', value: 'view' },
+                { title: 'Add Todo', value: 'add' },
+                { title: 'Delete', value: 'delete' },
+                { title: 'Exit', value: 'exit' }
+            ],
         })
     };
 
-    var message = await nextToDo();
-    console.log(message);
+    const addTodo = async() => {
+        return await prompts({
+            type: 'text',
+            name: 'value',
+            message: 'What is your new todo item?',
+        })
+    };
+    //var message = await nextToDo();
+    //console.log(message);
+    const showTodo = () => {
+        console.log("====================================");
+        todoList.forEach( (x) => console.log(x.value));
+        console.log("====================================");
+    }
+    
+    var option = {};
+    do {
+        option = await toDoMenu();
+        switch(option.value){
+            case 'view':
+                showTodo();
+                break;
+            case 'add':
+                todoList.push(await addTodo());
+                break;
+        }
+    } while (option.value != 'exit');
 };
 
-const message = main().catch(console.error);
+main().catch(console.error);
+
+
